92a8911c0b50e235888eb9a4bf6ea7d4b7f5d843
mindhackdiva <114305417+d0midigi@users.noreply.github.com>
2025-12-05 06:36:53 -0800

    Add toggle functionality for navigation bar

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+
+<script>
 // Dark mode toggle
     const toggle = document.getElementById('darkToggle');
 toggle.addEventListener('click', () => {
@@ -48,3 +50,22 @@ toggle.addEventListener('click', () => {
       );
       renderCards(filtered);
     });
+</script>
+
+<script>
+    const toggle = document.querySelector(".toggle");
+const nav_bar = document.querySelector(".table-of-contents");
+const list = document.querySelector(".table__list");
+
+toggle.addEventListener("click", ()=>{
+  if (toggle.textContent === "hide") {
+    toggle.textContent = "show";
+  }
+  else {
+    toggle.textContent = "hide";
+  }
+  
+  list.classList.toggle("list-invisible");
+  nav_bar.classList.toggle("table-narrow");
+});
+</script>
